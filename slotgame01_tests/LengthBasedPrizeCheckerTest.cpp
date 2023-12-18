e921817f5e6f9a2aa80b93afa7e86df8889cc1e7
Roman Zagorowski <roman.zagorowski@hotmail.com>
2023-12-18 07:19:37 +0100

    LengthBasedPrizeCheckerTest added

--- slotgame01_tests/LengthBasedPrizeCheckerTest.cpp
+++ slotgame01_tests/LengthBasedPrizeCheckerTest.cpp
@@ -4,6 +4,8 @@
 #include "../slotgame01/BetLines.h"
 #include "../slotgame01/LengthBasedPrizes.h"
 
+#include <algorithm>
+
 using namespace ::testing;
 
 TEST(LengthBasedPrizeCheckerTest, HandlesNoPrizeGameBoard)
@@ -45,4 +47,32 @@ TEST(LengthBasedPrizeCheckerTest, HandlesOnePrizeGameBoard)
     LengthBasedPayout expected_payout{ 7, 5, 25000, 1 };
 
     EXPECT_EQ(*payout, expected_payout);
+}
+
+TEST(LengthBasedPrizeCheckerTest, HandlesPrizesOnMultipleBetLines)
+{
+    GameBoard board{
+        5, 3,
+        {
+            2, 2, 2, 2, 2,
+            0, 0, 0, 0, 0,
+            3, 3, 3, 3, 3,
+        }
+    };
+
+    LengthBasedPrizeChecker checker{ length_based_prizes };
+
+    auto payouts = checker.CheckBetLines(betlines, board);
+
+    ASSERT_EQ(4, payouts.size());
+
+    LengthBasedPayout expected_payout_1{ 2, 5, 1000,  2 };
+    LengthBasedPayout expected_payout_2{ 3, 5, 1000,  3 };
+    LengthBasedPayout expected_payout_3{ 2, 3,  100, 20 };
+    LengthBasedPayout expected_payout_4{ 3, 3,  100, 19 };
+
+    EXPECT_NE(std::find(std::begin(payouts), std::end(payouts), expected_payout_1), std::end(payouts));
+    EXPECT_NE(std::find(std::begin(payouts), std::end(payouts), expected_payout_2), std::end(payouts));
+    EXPECT_NE(std::find(std::begin(payouts), std::end(payouts), expected_payout_3), std::end(payouts));
+    EXPECT_NE(std::find(std::begin(payouts), std::end(payouts), expected_payout_4), std::end(payouts));
 }
