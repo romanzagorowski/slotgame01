47f8cdc2f6afbaddf5cf00450d10089e48a35ee0
Roman Zagorowski <roman.zagorowski@hotmail.com>
2023-12-21 05:04:34 +0100

    CountBasedPrizeCheckerTest adjusted

--- slotgame01_tests/CountBasedPrizeCheckerTest.cpp
+++ slotgame01_tests/CountBasedPrizeCheckerTest.cpp
@@ -6,7 +6,31 @@
 
 #include <gtest/gtest.h>
 
-TEST(CountBasedPrizeCheckerTest, FindsTheBiggestSymbolSet)
+TEST(CountBasedPrizeCheckerTest, FindsFiveCrownSymbols)
+{
+    GameBoard board{
+        5, 3,
+        {
+            0, 1, 2, 0, 4,
+            5, 6, 0, 6, 5,
+            4, 0, 2, 1, 0
+        }
+    };
+
+    CountBasedPrizeChecker checker{
+        ___count_based_prizes
+    };
+
+    const auto payouts = checker.CheckGameBoard(board);
+
+    ASSERT_EQ(1, payouts.size());
+
+    CountBasedPayout expected_payout{ 0, 5, 5000 };
+
+    EXPECT_NE(std::find(std::begin(payouts), std::end(payouts), expected_payout), std::end(payouts));
+}
+
+TEST(CountBasedPrizeCheckerTest, FindsFourCrownSymbols)
 {
     GameBoard board{
         5, 3,
@@ -25,9 +49,51 @@ TEST(CountBasedPrizeCheckerTest, FindsTheBiggestSymbolSet)
 
     ASSERT_EQ(1, payouts.size());
 
-    CountBasedPayout     expected_payout{ 0, 4, 1000 };
-    CountBasedPayout not_expected_payout{ 0, 3,  200 };
+    CountBasedPayout expected_payout{ 0, 4, 1000 };
 
-    EXPECT_NE(std::find(std::begin(payouts), std::end(payouts),     expected_payout), std::end(payouts));
-    EXPECT_EQ(std::find(std::begin(payouts), std::end(payouts), not_expected_payout), std::end(payouts));
+    EXPECT_NE(std::find(std::begin(payouts), std::end(payouts), expected_payout), std::end(payouts));
+}
+
+TEST(CountBasedPrizeCheckerTest, FindsThreeCrownSymbols)
+{
+    GameBoard board{
+        5, 3,
+        {
+            0, 1, 2, 3, 4,
+            5, 6, 0, 6, 5,
+            4, 0, 2, 1, 4
+        }
+    };
+
+    CountBasedPrizeChecker checker{
+        ___count_based_prizes
+    };
+
+    const auto payouts = checker.CheckGameBoard(board);
+
+    ASSERT_EQ(1, payouts.size());
+
+    CountBasedPayout expected_payout{ 0, 3, 200 };
+
+    EXPECT_NE(std::find(std::begin(payouts), std::end(payouts), expected_payout), std::end(payouts));
+}
+
+TEST(CountBasedPrizeCheckerTest, FindsZeroCrownSymbols)
+{
+    GameBoard board{
+        5, 3,
+        {
+            7, 1, 2, 3, 4,
+            5, 6, 0, 6, 5,
+            4, 0, 2, 1, 4
+        }
+    };
+
+    CountBasedPrizeChecker checker{
+        ___count_based_prizes
+    };
+
+    const auto payouts = checker.CheckGameBoard(board);
+
+    ASSERT_EQ(0, payouts.size());
 }
